f1373da147e825f73d8b749e4f34959c234ec199
wangyongpeng <wangyongpeng@zhuanzhuan.com>
2022-03-15 16:29:05 +0800

    修改

--- startup.py
+++ startup.py
@@ -3,9 +3,9 @@
 
 #############################################
 # File Name: setup.py
-# Author: LiangjunFeng
-# Mail: zhumavip@163.com
-# Created Time:  2018-4-16 19:17:34
+# Author: wangyongpeng
+# Mail: 1690992651@qq.com
+# Created Time:  2022-03-15
 #############################################
 
 from setuptools import setup, find_packages            #这个包没有的可以pip一下
@@ -13,19 +13,19 @@ from setuptools import setup, find_packages            #这个包没有的可以
 setup(
     name="BucketUtils",      #这里是pip项目发布的名称
     version="1.1.0",  #版本号，数值大的会优先被pip
-    keywords=("pip", "SICA","featureextraction"),
-    description="An feature extraction algorithm",
-    long_description="An feature extraction algorithm, improve the FastICA",
+    keywords=("pip", "wangyongpeng","tree"),
+    description="用树模型代替qcut进行分桶。",
+    long_description="用决策树来分桶",
     license="MIT Licence",
 
-    url="https://github.com/LiangjunFeng/SICA",     #项目相关文件地址，一般是github
-    author="LiangjunFeng",
-    author_email="zhumavip@163.com",
+    url="https://github.com/HeiBoWang/DecisionTreeBktUtil.git",     # 项目相关文件地址，一般是github
+    author="wangyongpeng",
+    author_email="1690992651@qq.com",
 
     packages=find_packages(),
-    include_package_data = True,
+    include_package_data=True,
     platforms="any",
-    install_requires=["numpy"]          #这个项目需要的第三方库
+    install_requires=["numpy"]          # 这个项目需要的第三方库
 )
 
 
