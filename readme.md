2f0f4de65f8a65883e61045b0e2f516c1788362d
wangyongpeng <wangyongpeng@zhuanzhuan.com>
2022-04-06 17:15:56 +0800

    toad包来进行分桶

--- readme.md
+++ readme.md
@@ -1,5 +1,20 @@
 ## DecisionTreeBktUtil 使用教程
-### step 1: 
+### step 1: 导包
 * pip install DecisionTreeBktUtil -i –trusted-host https://pypi.python.org/simple
 
-### step 2:
+### step 2: 引用实例
+
+        # 导包
+        from DecisionTreeBktUtil import getColBktFromDF
+        import pandas as pd
+        # 读取csv数据
+        df_train = pd.read_csv("./hh.csv",header=0,sep='	')
+        # 指定要分桶的列名
+        featureCols = ['price']
+        # 遍历列名，自动分桶
+        for col in featureCols:
+            # 在这个地方使用分桶工具
+            getColBktFromDF.getFeatureBkt_DT(df_train, col, 'click_label')
+
+### step 3: 拷贝输出的结果
+        'price': [0.0, 933.0, 943.0]
--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@
 from setuptools import setup, find_packages            #这个包没有的可以pip一下
 
 setup(
-    name="DecisionTreeBktUtil",      #这里是pip项目发布的名称
-    version="1.1.1",  #版本号，数值大的会优先被pip
+    name="ZHUANZHUANBktUtil",      #这里是pip项目发布的名称
+    version="2.1.1",  #版本号，数值大的会优先被pip
     keywords=("pip", "wangyongpeng","tree"),
-    description="用树模型代替qcut进行分桶。",
-    long_description="用决策树来分桶",
+    description="toad包来进行分桶。",
+    long_description="toad包来进行分桶",
     license="BSD Licence",
 
     url="https://github.com/HeiBoWang/DecisionTreeBktUtil",     # 项目相关文件地址，一般是github
@@ -24,8 +24,7 @@ setup(
 
     packages=find_packages(),
     include_package_data=True,
-    platforms="any",
-    install_requires=["numpy"]          # 这个项目需要的第三方库
+    platforms="any"
 )
 
 
